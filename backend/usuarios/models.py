f6048f13516499635f2f6215f69eb507c1a4e61c
Tawan <ntctawan@gmail.com>
2025-12-14 13:29:25 -0300

    criando modelo de usuario

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    TIPO_USUARIO = (
+        ('coordenador', 'Coordenador'),
+        ('professor', 'Professor'),
+        ('estudante', 'Estudante'),
+        ('visitante', 'Visitante'),
+    )
+    tipo_usuario = models.CharField(max_length=20, choices=TIPO_USUARIO)
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.nome} ({self.tipo_usuario})"
