33de7bcbf8b193c1ffb93bdb6ff818bbe718d340
Tawan <ntctawan@gmail.com>
2025-12-14 13:48:18 -0300

    Cria view de perfil de usuário autenticado em views.py

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Usuario
+from .serializers import UsuarioSerializer
 
-# Create your views here.
+class PerfilUsuarioView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UsuarioSerializer(request.user)
+        return Response(serializer.data)
