d8b0ca057cf666c3d8fa1bc61a3eb957b4b3c79d
ezequielfarias144 <ezequielfarias144@gmail.com>
2025-12-14 22:03:46 -0300

    adicionei admin.py do projetos

--- backend/projetos/admin.py
+++ backend/projetos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from projetos.models import Projeto
 
 # Register your models here.
+@admin.register(Projeto)
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'status', 'data_inicio', 'data_fim_prevista']
+    search_fields = ['nome', 'descricao']
+    readonly_fields = ['id']
+    ordering = ['data_inicio']
